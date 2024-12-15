aadc885b79d5769de6526d211adcf3bc88b17161
Abdullah khan <akhan283785@gmail.com>
2024-12-16 00:11:29 +0530

    try

--- script.js
+++ script.js
@@ -3,9 +3,18 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+  //Add your code here
+  let totalPrice = 0;
+  let table = document.querySelector("table tbody");
+  let tr = document.createElement("tr");
+  let td = document.createElement("td");
+  let itemsPrice = document.querySelectorAll(".price");
+  for (let t of itemsPrice) {
+    totalPrice += +t.innerHTML;
+  }
+  td.innerHTML = totalPrice;
+  tr.append(td);
+  table.append(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
